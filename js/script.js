812d3095340b17236ead53add3173f2449db0d04
Charles Atchison <Charles.Atchison@outlook.com>
2023-09-05 20:17:02 -0500

    changes to js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
-// script.js
+console.log("Script started"); // Debugging: Script start
 
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
+console.log("Tablinks and Tabcontents:", tablinks, tabcontents); // Debugging: Check if tablinks and tabcontents are found
+
 function opentab(event, tabname) {
+    console.log("opentab function called with tabname:", tabname); // Debugging: opentab function call
     for (let tablink of tablinks) {
         tablink.classList.remove("active-link");
     }
@@ -15,64 +18,56 @@ function opentab(event, tabname) {
 }
 
 function openmenu() {
+    console.log("openmenu function called"); // Debugging: openmenu function call
     var sidemenu = document.getElementById("sidemenu");
     sidemenu.style.right = "0";
 }
 
 function closemenu() {
+    console.log("closemenu function called"); // Debugging: closemenu function call
     var sidemenu = document.getElementById("sidemenu");
     sidemenu.style.right = "-200px";
 }
 
-// Debugging Step 4: EmailJS Initialization
+// EmailJS Initialization
 (function(){
     emailjs.init("C_1ovVKS4xIsyq0yV");
-    console.log('EmailJS initialized'); // Debugging Step 7: Manual Testing
- })();
+    console.log("EmailJS initialized"); // Debugging: EmailJS Initialization
+})();
 
 document.addEventListener('DOMContentLoaded', function() {
-    // ...existing code for loading header, copyright, and contact_me...
+    console.log("DOM fully loaded and parsed"); // Debugging: DOMContentLoaded
 
-    // Debugging Step 2: Form Element
     const form = document.getElementById('contact-form');
+    console.log("Form element:", form); // Debugging: Check if form is found
     
-    if (form) { // Check if the form exists on the page
-        console.log('Form element found'); // Debugging Step 7: Manual Testing
+    if (form) {
+        console.log("Form element found");
 
-        // Debugging Step 5: Event Listener
         form.addEventListener('submit', function(event) {
+            console.log("Form submit event triggered"); // Debugging: Form submit event
             event.preventDefault();
-            console.log('Form submitted'); // Debugging Step 7: Manual Testing
 
-            // Create an object to hold the form data
             const templateParams = {
                 name: form.name.value,
                 email: form.email.value,
                 message: form.message.value
             };
-            
-            // Debugging Step 8: Template ID
-            // Use emailjs.send() method
+            console.log("Template Parameters:", templateParams); // Debugging: Template Parameters
+
             emailjs.send('service_7unekk1', 'template_u4rubzs', templateParams)
                 .then(function(response) {
                     console.log('SUCCESS!', response.status, response.text);
-                    
-                    // Display success message
                     alert('Yay, your message was sent!');
-                    
-                    // Refresh the page after 5 seconds
                     setTimeout(function() {
                         location.reload();
                     }, 5000);
-                    
                 }, function(error) {
                     console.log('FAILED...', error);
-                    
-                    // Display failure message
                     alert('There was some issue, sorry, please try again.');
                 });
         });
     } else {
-        console.log('Form element not found'); // Debugging Step 7: Manual Testing
+        console.log("Form element not found");
     }
 });
